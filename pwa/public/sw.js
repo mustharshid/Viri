ed39267d3639c83c9566e587953e478a37a0bfd4
M Mustharshid <mustharshid@gmail.com>
2026-07-04 22:22:05 +0500

    build: compile assets, package extension, and deploy

---
+++ pwa/public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'viri-admin-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
